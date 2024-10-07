ffd5317a9c2137bd7753e654cdb5d8962ea2878f
shimn34 <abeshimpei34@gmail.com>
2024-10-07 16:02:50 +0900

    ナビゲーションメニューの作成

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
-import deveLogo from '../../public/assets/dove.png';
 import '../../style.css';
 
 export default function Header() {
     return (
         <header>
             <h1>Birdwatching</h1>
-            <img src={deveLogo} />
+            <img src="../../public/assets/dove.png" alt="a simple dove logo" />
+
+            <nav>
+                <ul>
+                    <li><span>Home</span></li>
+                    <li><a href="#">Get started</a></li>
+                    <li><a href="#">Photos</a></li>
+                    <li><a href="#">Gear</a></li>
+                    <li><a href="#">Forum</a></li>
+                </ul>
+            </nav>
         </header>
     );
 };
