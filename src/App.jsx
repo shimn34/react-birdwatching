b41a051c3712eb29e55d5cb8861d40c27e45ae27
shimn34 <abeshimpei34@gmail.com>
2024-10-09 14:00:32 +0900

    App.jsxの修正

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Header from "./components/Header";
-import Main from "./components/main";
+import Main from "./components/Main";
 import Footer from "./components/Footer"
 
 export default function App() {
