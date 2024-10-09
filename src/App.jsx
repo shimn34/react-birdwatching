83b1aad3998114f20caf9a69178ee8a227c8f94a
shimn34 <abeshimpei34@gmail.com>
2024-10-09 13:40:30 +0900

    フッターの追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import Main from "./components/main";
+import Footer from "./components/Footer"
 
 export default function App() {
     return (
         <div>
             <Header />
             <Main />
+            <Footer />
         </div>
     );
 }
